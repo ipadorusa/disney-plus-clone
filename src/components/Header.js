a6bfdc9dc93dced4e821b8113be1eba1e78641c6
ipadorusa <ipadorusa@gmail.com>
2021-05-06 23:15:04 +0900

    20210506

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,12 +2,95 @@ import React from 'react';
 import styled from 'styled-components';
 
 function Header() {
-  return <Container>header</Container>;
+  return (
+    <Nav>
+      <Logo src='/images/logo.svg' />
+      <NavMenu>
+        <a>
+          <img src='/images/home-icon.svg' />
+          <span>HOME</span>
+        </a>
+        <a>
+          <img src='/images/search-icon.svg' />
+          <span>SEARCH</span>
+        </a>
+        <a>
+          <img src='/images/watchlist-icon.svg' />
+          <span>WATCHLIST</span>
+        </a>
+        <a>
+          <img src='/images/original-icon.svg' />
+          <span>ORIGINALS</span>
+        </a>
+        <a>
+          <img src='/images/movie-icon.svg' />
+          <span>MOVIES</span>
+        </a>
+        <a>
+          <img src='/images/series-icon.svg' />
+          <span>SERIES</span>
+        </a>
+      </NavMenu>
+      <UserImg src='https://scontent-ssn1-1.xx.fbcdn.net/v/t1.6435-1/p480x480/48413277_1628706597230489_4429624566658105344_n.jpg?_nc_cat=107&ccb=1-3&_nc_sid=7206a8&_nc_ohc=rO-DmKVL2eAAX9m6O6z&_nc_ht=scontent-ssn1-1.xx&tp=6&oh=da40b0dd3d57db167ce49ba4dda68169&oe=60BA45C2' />
+    </Nav>
+  );
 }
 
 export default Header;
 
-const Container = styled.div`
+const Nav = styled.div`
   height: 70px;
   background-color: #090b13;
+  display: flex;
+  align-items: center;
+  padding: 0 36px;
+`;
+
+const Logo = styled.img`
+  width: 80px;
+`;
+const NavMenu = styled.div`
+  display: flex;
+  flex: 1;
+  margin-left: 25px;
+  align-items: center;
+  a {
+    display: flex;
+    align-items: center;
+    padding: 0 12px;
+    cursor: pointer;
+    img {
+      height: 20px;
+    }
+
+    span {
+      font-size: 13px;
+      letter-spacing: 1.42px;
+      position: relative;
+      &:after {
+        content: '';
+        height: 2px;
+        background: white;
+        position: absolute;
+        left: 0;
+        right: 0;
+        bottom: -6px;
+        opacity: 0;
+        transform: scaleX(0);
+        transition: all 250ms cubic-bezier(0.25, 0.46, 0.45, 0.94) 0s;
+      }
+    }
+    &:hover {
+      span:after {
+        transform: scaleX(1);
+        opacity: 1;
+      }
+    }
+  }
+`;
+const UserImg = styled.img`
+  width: 48px;
+  height: 48px;
+  border-radius: 50%;
+  cursor: pointer;
 `;
