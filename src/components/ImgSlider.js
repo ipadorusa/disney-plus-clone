89f1c96a2295ae8af9bc6ed539544cba8cd844ff
ipadorusa <ipadorusa@gmail.com>
2021-05-08 00:00:01 +0900

    20210507

---
+++ src/components/ImgSlider.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import styled from 'styled-components';
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+
+function ImgSlider() {
+  let setting = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+  };
+  return (
+    <Carousel {...setting}>
+      <Wrap>
+        <img src='/images/slider-badging.jpg'></img>
+      </Wrap>
+      <Wrap>
+        <img src='/images/slider-badging.jpg'></img>
+      </Wrap>
+    </Carousel>
+  );
+}
+
+export default ImgSlider;
+const Carousel = styled(Slider)``;
+const Wrap = styled.div``;
