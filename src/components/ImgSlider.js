51e018dd7c4d2acc3efe01a099040c75fdd47f65
ipadorusa <ipadorusa@gmail.com>
2021-05-11 20:50:01 +0900

    20210511

--- src/components/ImgSlider.js
+++ src/components/ImgSlider.js
@@ -11,7 +11,7 @@ function ImgSlider() {
     speed: 500,
     slidesToShow: 1,
     slidesToScroll: 1,
-    autoplay: true,
+    autoplay: false,
   };
   return (
     <Carousel {...setting}>
@@ -19,12 +19,44 @@ function ImgSlider() {
         <img src='/images/slider-badging.jpg'></img>
       </Wrap>
       <Wrap>
-        <img src='/images/slider-badging.jpg'></img>
+        <img src='/images/slider-badag.jpg'></img>
       </Wrap>
     </Carousel>
   );
 }
 
 export default ImgSlider;
-const Carousel = styled(Slider)``;
-const Wrap = styled.div``;
+const Carousel = styled(Slider)`
+  margin-top: 20px;
+
+  ul li button {
+    &:before {
+      font-size: 10px;
+      color: rgba(150, 158, 171);
+    }
+  }
+  li.slick-avtice button::before {
+    color: white;
+  }
+  .slick-list {
+    overflow: visible;
+  }
+  button {
+    z-index: 1;
+  }
+`;
+const Wrap = styled.div`
+  cursor: pointer;
+  img {
+    border: 4px solid transparent;
+    border-radius: 4px;
+    width: 100%;
+    height: 100%;
+    box-shadow: rgba(0 0 0 / 69%) 0px 26px 30px - 10px,
+      rgba(0 0 0 / 73%) 0 16px 10px -10px;
+    transition-duration: 300ms;
+    &:hover {
+      border: 4px solid rgba(249, 249, 249, 0.8);
+    }
+  }
+`;
